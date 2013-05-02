f69c261745c8092081407cf05325cdf249a80879
Herbert G. Fischer <herbert.fischer@gmail.com>
2013-05-01 23:40:22 -0300

    Added some examples migrated from C counterparts

---
+++ examples/extend/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/gobinds/imagick/imagick"
+)
+
+func main() {
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	var err error
+
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	pw := imagick.NewPixelWand()
+	defer pw.Destroy()
+	pw.SetColor("blue")
+
+	err = mw.ReadImage("logo:")
+	if err != nil {
+		panic(err)
+	}
+
+	w := int(mw.GetImageWidth())
+	h := int(mw.GetImageHeight())
+	mw.SetImageBackgroundColor(pw)
+
+	// This centres the original image on the new canvas.
+	// Note that the extent's offset is relative to the
+	// top left corner of the *original* image, so adding an extent
+	// around it means that the offset will be negative
+	err = mw.ExtentImage(1024, 768, -(1024-w)/2, -(768-h)/2)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the compression quality to 95 (high quality = low compression)
+	err = mw.SetImageCompressionQuality(95)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write the new image
+	err = mw.WriteImage("logo_extent.jpg")
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ examples/resize/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gobinds/imagick/imagick"
+)
+
+func main() {
+	imagick.Initialize()
+	// Schedule cleanup
+	defer imagick.Terminate()
+	var err error
+
+	mw := imagick.NewMagickWand()
+	// Schedule cleanup
+	defer mw.Destroy()
+
+	err = mw.ReadImage("logo:")
+	if err != nil {
+		panic(err)
+	}
+
+	// Get original logo size
+	width := mw.GetImageWidth()
+	height := mw.GetImageHeight()
+
+	// Calculate half the size
+	hWidth := uint(width / 2)
+	hHeight := uint(height / 2)
+
+	// Resize the image using the Lanczos filter
+	// The blur factor is a float, where > 1 is blurry, < 1 is sharp
+	err = mw.ResizeImage(hWidth, hHeight, imagick.LanczosFilter, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the compression quality to 95 (high quality = low compression)
+	err = mw.SetImageCompressionQuality(95)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write the new image
+	err = mw.WriteImage("logo_resize.jpg")
+	if err != nil {
+		panic(err)
+	}
+}
