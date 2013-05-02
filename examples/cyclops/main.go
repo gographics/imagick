60015b281e60c7ab32c8c75a4d871d94fdcd0462
Herbert G. Fischer <herbert.fischer@gmail.com>
2013-05-02 00:15:54 -0300

    Add the cyclops example

---
+++ examples/cyclops/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/gobinds/imagick/imagick"
+	"os"
+)
+
+func main() {
+	var err error
+	imagick.Initialize()
+	defer imagick.Terminate()
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+	bg := imagick.NewPixelWand()
+	defer bg.Destroy()
+	fg := imagick.NewPixelWand()
+	defer fg.Destroy()
+
+	err = mw.ReadImage("http://www.imagemagick.org/Usage/images/cyclops_sm.gif")
+	if err != nil {
+		panic(err)
+	}
+
+	bg.SetColor("white")
+	mw.BorderImage(bg, 1, 1)
+	mw.SetImageAlphaChannel(imagick.SetAlphaChannel)
+
+	fg.SetColor("none")
+	channel := imagick.RGBChannels | imagick.AlphaChannel
+
+	// Floodfill the "background" colour with the "foreground" colour
+	// starting at coordinate 0,0 using a fuzz of 20
+	mw.FloodfillPaintImage(channel, fg, 20, bg, 0, 0, false)
+	mw.ShaveImage(1, 1)
+
+	mw.DisplayImage(os.Getenv("DYSPLAY"))
+	if err != nil {
+		panic(err)
+	}
+}
