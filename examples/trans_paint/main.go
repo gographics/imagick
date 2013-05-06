d27871397a9dbdce5f4fd6549f7fa78d479642d3
Herbert G. Fischer <herbert.fischer@gmail.com>
2013-05-05 23:41:02 -0300

    Add trans_paint example

---
+++ examples/trans_paint/main.go
@@ -0,0 +1,27 @@
+// Port of http://members.shaw.ca/el.supremo/MagickWand/trans_paint.htm to Go
+package main
+
+import "github.com/gographics/imagick/imagick"
+
+func main() {
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+	mw.ReadImage("logo:")
+
+	// A larger fuzz value allows more colours "near" white to be
+	// modified. A fuzz of zero only allows an exact match with the
+	// given colour
+	// Set up the pixelwand containing the colour to be "targeted"
+	// by transparency
+	target := imagick.NewPixelWand()
+	defer target.Destroy()
+	target.SetColor("white")
+	// Change the transparency of all colours which match target (with
+	// fuzz applied). In this case they are made completely transparent (0)
+	// but you can set this to any value from 0 to 1.
+	mw.TransparentPaintImage(target, 0, 10, false)
+	mw.WriteImage("logo_white.png")
+}
