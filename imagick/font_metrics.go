fc5052b180566d53a0e3d9acd077ba44c73f56d7
Herbert G. Fischer <herbert.fischer@gmail.com>
2013-05-06 00:01:29 -0300

    Add MIT license

--- imagick/font_metrics.go
+++ imagick/font_metrics.go
@@ -1,3 +1,7 @@
+// Copyright 2013 Herbert G. Fischer. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package imagick
 
 type FontMetrics struct {
