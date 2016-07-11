97751fce9fb77c48170842d3f1131c5ba24546bd
Danack <Danack@basereality.com>
2016-07-11 14:25:35 +0000

    Added first draft of kernel info functions

---
+++ imagick/geometry_info.go
@@ -0,0 +1,12 @@
+package imagick
+
+/*
+#include <magick/MagickCore.h>
+#include <magick/geometry.h>
+#include <magick/morphology.h>
+*/
+import "C"
+
+type GeometryInfo struct {
+    gi C.GeometryInfo
+}
