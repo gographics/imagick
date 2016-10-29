4ffd31dd827f17f33bcefae39d166804d432b897
Justin Israel <justinisrael@gmail.com>
2016-10-29 17:32:22 +1300

    Fix memory management of KernelInfo; extend methods; add tests; gofmt

--- examples/morphology/main.go
+++ examples/morphology/main.go
@@ -1,35 +1,30 @@
 // Port of http://members.shaw.ca/el.supremo/MagickWand/grayscale.htm to Go
 package main
 
-/*
-#cgo CFLAGS: -I/usr/local/include/ImageMagick-6
-#cgo LDFLAGS: -L/usr/sbin/lib
-*/
-
 import (
 	"fmt"
-    "gopkg.in/gographics/imagick.v2/imagick"
+
+	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
 func image_blur() {
-    mw := imagick.NewMagickWand()
-    mw.SetSize(640, 480);
-    mw.ReadImage("magick:logo");
-    mw.SetFormat("png");
+	mw := imagick.NewMagickWand()
+	mw.SetSize(640, 480)
+	mw.ReadImage("magick:logo")
+	mw.SetFormat("png")
 
-    kernel_info := imagick.AcquireKernelBuiltIn(imagick.KERNEL_RING, "2,1")
-    kernel_info.Scale(1.0, imagick.KERNEL_NORMALIZE_VALUE);
-    kernel_values := kernel_info.ToArray();
+	kernel_info := imagick.NewKernelInfoBuiltIn(imagick.KERNEL_RING, "2,1")
+	kernel_info.Scale(1.0, imagick.KERNEL_NORMALIZE_VALUE)
+	kernel_values := kernel_info.ToArray()
 
-    fmt.Println(kernel_values)
-    mw.WriteImage("testLogoBefore.png");
-    mw.MorphologyImage(imagick.MORPHOLOGY_CONVOLVE, 1, kernel_info);
-    mw.WriteImage("testLogoBlurred.png");
+	fmt.Println(kernel_values)
+	mw.WriteImage("testLogoBefore.png")
+	mw.MorphologyImage(imagick.MORPHOLOGY_CONVOLVE, 1, kernel_info)
+	mw.WriteImage("testLogoBlurred.png")
 }
-
 
 func main() {
 	imagick.Initialize()
 	defer imagick.Terminate()
-    image_blur();
+	image_blur()
 }
--- imagick/geometry_info.go
+++ imagick/geometry_info.go
@@ -2,11 +2,9 @@ package imagick
 
 /*
 #include <magick/MagickCore.h>
-#include <magick/geometry.h>
-#include <magick/morphology.h>
 */
 import "C"
 
 type GeometryInfo struct {
-    gi C.GeometryInfo
+	gi C.GeometryInfo
 }
