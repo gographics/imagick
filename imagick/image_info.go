45802a09691e54477f2f675b3f862ce95e5783a1
Justin Israel <justinisrael@gmail.com>
2016-10-29 16:57:29 +1300

    Fix memory management of ImageInfo

--- imagick/image_info.go
+++ imagick/image_info.go
@@ -8,17 +8,22 @@ package imagick
 #include <MagickCore/MagickCore.h>
 */
 import "C"
+import "runtime"
 
 type ImageInfo struct {
 	info *C.ImageInfo
 }
 
 func newImageInfo() *ImageInfo {
-	ii := C.AcquireImageInfo()
-	C.GetImageInfo(ii)
-	return &ImageInfo{ii}
+	ptr := C.AcquireImageInfo()
+	C.GetImageInfo(ptr)
+	imageInfo := &ImageInfo{ptr}
+	runtime.SetFinalizer(imageInfo, Destroy)
+	return imageInfo
 }
 
+// Destroy the ImageInfo immediately.
+// This will also be called automatically during garbage collection.
 func (ii *ImageInfo) Destroy() {
 	if ii.info != nil {
 		C.DestroyImageInfo(ii.info)
