52135f9502fa3ae287c9d1ec9bf84bb83153fdd5
Justin Israel <justinisrael@gmail.com>
2018-12-30 19:57:33 +1300

    Add a test script to confirm all examples can build

---
+++ examples/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+fail=0
+failed=()
+
+for example in $(find . -type f -name main.go | sort); do
+	path=$(realpath $example)
+	name=$(basename $(dirname $path))
+	echo $path
+	if ! go build -o ${DIR}/tmp/${name} ${path} ; then
+		fail=1
+		failed+=($name)
+	fi
+done
+
+if (( $fail == 1 )); then
+	echo "================"
+	echo "Failed examples:"
+	printf '  %s\n' "${failed[@]}"
+fi
+
+exit $fail
