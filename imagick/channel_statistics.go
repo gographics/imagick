e61861a6ad0fb7a4b43a56de82a9305729139c1b
Justin Israel <justinisrael@gmail.com>
2024-01-08 16:00:31 +1300

    Disable unused C field causing type error in ImageMagick 7.1.1-24 (fixes #301)

--- imagick/channel_statistics.go
+++ imagick/channel_statistics.go
@@ -13,5 +13,13 @@ import "C"
 //"unsafe"
 
 type ChannelStatistics struct {
-	cs *C.ChannelStatistics
+	// TODO: https://github.com/gographics/imagick/issues/301
+	//   As of ImageMagick 7.1.1-24 they have introduced fields
+	//   of type "long double". This causes a cgo compilation error.
+	//   Disabling this reference for now until the struct is actually
+	//   used somewhere in these bindings.
+	//   When needed, A c-shim type will need to be declared to mirror
+	//   ChannelStatistics with a double instead of long double fields,
+	//   and a helper to convert between.
+	//cs *C.ChannelStatistics
 }
