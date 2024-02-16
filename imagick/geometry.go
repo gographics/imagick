be0c86028e0e95b2f4c458b5a6aac4221e55343e
Jeremy Collins <jeremy.collins@beetlebug.org>
2024-02-15 22:01:18 -0500

    More accurately wrap Parse*Geometry APIs

--- imagick/geometry.go
+++ imagick/geometry.go
@@ -13,55 +13,74 @@ func ParseGeometry(geometry string, info *GeometryInfo) uint {
 	return uint(flags)
 }
 
+func SetGeometry(image *Image, info *RectangleInfo) {
+	var ri C.RectangleInfo
+
+	C.SetGeometry(image.img, &ri)
+	*info = *newRectangleInfo(&ri)
+}
+
 func ParseAbsoluteGeometry(geometry string, info *RectangleInfo) uint {
-	var gi C.RectangleInfo
-	flags := C.ParseAbsoluteGeometry(C.CString(geometry), &gi)
-	*info = *newRectangleInfo(&gi)
+	var ri C.RectangleInfo
+
+	ri.x = C.ssize_t(info.X)
+	ri.y = C.ssize_t(info.Y)
+	ri.width = C.size_t(info.Width)
+	ri.height = C.size_t(info.Height)
+
+	flags := C.ParseAbsoluteGeometry(C.CString(geometry), &ri)
+	*info = *newRectangleInfo(&ri)
+
 	return uint(flags)
 }
 
 func ParseMetaGeometry(geometry string, x *int, y *int, width *uint, height *uint) uint {
-	var info C.RectangleInfo
+	var ri C.RectangleInfo
 
-	flags := C.ParseMetaGeometry(C.CString(geometry), &info.x, &info.y, &info.width, &info.height)
+	ri.x = C.ssize_t(*x)
+	ri.y = C.ssize_t(*y)
+	ri.width = C.size_t(*width)
+	ri.height = C.size_t(*height)
 
-	*x = int(info.x)
-	*y = int(info.y)
-	*width = uint(info.width)
-	*height = uint(info.height)
+	flags := C.ParseMetaGeometry(C.CString(geometry), &ri.x, &ri.y, &ri.width, &ri.height)
+
+	*x = int(ri.x)
+	*y = int(ri.y)
+	*width = uint(ri.width)
+	*height = uint(ri.height)
 
 	return uint(flags)
 }
 
-func (mw *MagickWand) ParseGravityGeometry(geometry string, rect *RectangleInfo, exception *ExceptionInfo) uint {
-	var info C.RectangleInfo
+func ParseGravityGeometry(image *Image, geometry string, rect *RectangleInfo, exception *ExceptionInfo) uint {
+	var ri C.RectangleInfo
 	var ex C.ExceptionInfo
 
-	image := C.GetImageFromMagickWand(mw.mw)
-	flags := C.ParseGravityGeometry(image, C.CString(geometry), &info, &ex)
-	*rect = *newRectangleInfo(&info)
+	flags := C.ParseGravityGeometry(image.img, C.CString(geometry), &ri, &ex)
+	*rect = *newRectangleInfo(&ri)
 	*exception = *newExceptionInfo(&ex)
+
 	return uint(flags)
 }
 
-func (mw *MagickWand) ParsePageGeometry(geometry string, rect *RectangleInfo, exception *ExceptionInfo) uint {
-	var info C.RectangleInfo
+func ParsePageGeometry(image *Image, geometry string, rect *RectangleInfo, exception *ExceptionInfo) uint {
+	var ri C.RectangleInfo
 	var ex C.ExceptionInfo
 
-	image := C.GetImageFromMagickWand(mw.mw)
-	flags := C.ParsePageGeometry(image, C.CString(geometry), &info, &ex)
-	*rect = *newRectangleInfo(&info)
+	flags := C.ParsePageGeometry(image.img, C.CString(geometry), &ri, &ex)
+	*rect = *newRectangleInfo(&ri)
 	*exception = *newExceptionInfo(&ex)
+
 	return uint(flags)
 }
 
-func (mw *MagickWand) ParseRegionGeometry(geometry string, rect *RectangleInfo, exception *ExceptionInfo) uint {
-	var info C.RectangleInfo
+func ParseRegionGeometry(image *Image, geometry string, rect *RectangleInfo, exception *ExceptionInfo) uint {
+	var ri C.RectangleInfo
 	var ex C.ExceptionInfo
 
-	image := C.GetImageFromMagickWand(mw.mw)
-	flags := C.ParseRegionGeometry(image, C.CString(geometry), &info, &ex)
-	*rect = *newRectangleInfo(&info)
+	flags := C.ParseRegionGeometry(image.img, C.CString(geometry), &ri, &ex)
+	*rect = *newRectangleInfo(&ri)
 	*exception = *newExceptionInfo(&ex)
+
 	return uint(flags)
 }
